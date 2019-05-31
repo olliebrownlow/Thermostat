18483dfb865f2ad9f13118a42f9960acdea11754
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-31 13:07:38 +0100

    refactors out magic numbers

--- jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine-standalone-3.4.0/src/Thermostat.js
@@ -3,6 +3,10 @@
 function Thermostat() {
   this._temp = 20;
   this._isPowerSavingMode = true;
+  this._DEFAULT_TEMP = 20;
+  this._MAX_TEMP_PSM_ON = 25;
+  this._MAX_TEMP_PSM_OFF = 32;
+  this._MIN_TEMP = 10;
 }
 
 Thermostat.prototype.temp = function() {
@@ -14,23 +18,22 @@ Thermostat.prototype.powerSavingMode = function() {
 }
 
 Thermostat.prototype.up = function() {
-  if (this._temp === 32 && this._isPowerSavingMode === false) throw "Maximum temp is 32 degrees";
-  if (this._temp === 25 && this._isPowerSavingMode === true) throw "Power Saving Mode is on: Maximum temp is 25 degrees";
+  if (this._temp === this._MAX_TEMP_PSM_OFF && this._isPowerSavingMode === false) throw "Maximum temp is 32 degrees";
+  if (this._temp === this._MAX_TEMP_PSM_ON && this._isPowerSavingMode === true) throw "Power Saving Mode is on: Maximum temp is 25 degrees";
   return this._temp++;
 }
 
 Thermostat.prototype.down = function() {
-  if (this._temp === 10) throw "Minimum temperature is 10 degrees";
+  if (this._temp === this._MIN_TEMP) throw "Minimum temperature is 10 degrees";
   this._temp--;
 }
 
 Thermostat.prototype.psmSwitch = function() {
-  if (this._isPowerSavingMode === false) return this._isPowerSavingMode = true;
-  if (this._isPowerSavingMode === true) return this._isPowerSavingMode = false;
+  this._isPowerSavingMode = !this._isPowerSavingMode;
 }
 
 Thermostat.prototype.reset = function() {
-  this._temp = 20;
+  this._temp = this._DEFAULT_TEMP;
 }
 
 Thermostat.prototype.currentUsage = function () {
